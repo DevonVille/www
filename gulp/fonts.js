d403887b50cbefe68ec41954eacdd379ab7990e2
iwfmp <iwfmp@yandex.com>
2017-02-17 09:31:39 +0200

    fix(general): gulp config and other stuff

--- gulp/fonts.js
+++ gulp/fonts.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 
 // SVG optimization task
 gulp.task('fonts', function () {
-  return gulp.src('src/fonts/')
+  return gulp.src('src/fonts/**/*.*')
     .pipe(gulp.dest('.cache/staging/fonts'));
 });
--- gulp/revision.js
+++ gulp/revision.js
@@ -3,8 +3,8 @@ var rev = require('gulp-rev');
 var del = require('rev-del');
 var path = require('path');
 
-gulp.task('revision', ['styles','scripts', 'images', 'svg'], function() {
-    return gulp.src(['.cache/staging/css/**/*.css', '.cache/staging/js/**/*.js', '.cache/staging/img/**/*.*', '.cache/staging/svg/**/*.svg'], {base: path.join(process.cwd(), '.cache/staging')})
+gulp.task('revision', ['styles','scripts', 'images', 'svg', 'fonts'], function() {
+    return gulp.src(['.cache/staging/css/**/*.css', '.cache/staging/js/**/*.js', '.cache/staging/img/**/*.*', '.cache/staging/svg/**/*.svg', '.cache/staging/fonts/**/*.css'], {base: path.join(process.cwd(), '.cache/staging')})
         .pipe(rev())
         .pipe(gulp.dest('hugo/static'))
         .pipe(rev.manifest())
