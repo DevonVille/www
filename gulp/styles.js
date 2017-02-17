a6b2bd3a9b64543ef1c75747822b322e13627d81
iwfmp <iwfmp@yandex.com>
2017-02-17 13:04:34 +0200

    fix(general): plumber gulp and adjustments to nav

--- gulp/styles.js
+++ gulp/styles.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var cleancss = require('gulp-clean-css');
+var plumber = require('gulp-plumber');
 
 gulp.task('styles', function() {
     return gulp.src('src/styles/**/*.scss')
+        .pipe(plumber())
         .pipe(sass())
         .pipe(autoprefixer('last 2 version'))
         .pipe(cleancss({advanced:false}))
